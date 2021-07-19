997db443fa7cace24ae60abc1435dfe9bda87174
flareflo <florianlipp17@gmail.com>
2021-07-19 14:27:54 +0200

    Added messaging compiles = 0

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,7 @@ use serenity::model::prelude::Ready;
 use serenity::async_trait;
 use serenity::model::channel::Message;
 
-
-#[tokio::main]
 async fn main() {
-    let html = Html::parse_document(&get("https://warthunder.com/en/news/").await.unwrap().text().await.unwrap());
-    let top_article_selector = "#bodyRoot > div.content > div:nth-child(2) > div > div > section > div > div.showcase__content-wrapper > div:nth-child(1)";
-    let top_article = html.select(&Selector::parse(top_article_selector).unwrap()).next().unwrap().text().collect::<String>();
-
-    if top_article.contains("Shooting") {
-        println!("yes");
-    }
 
     let token = read_to_string("/home/flareflo/CLionProjects/WT_event_handler/assets/discord_token.txt").unwrap();
 
@@ -34,6 +25,15 @@ struct Handler;
 impl EventHandler for Handler {
     async fn ready(&self, ctx: Context, data_about_bot: Ready) {
         println!("{} fucked your mom", data_about_bot.user.name);
-        ChannelId::from(866634236232597534).say(&ctx.http, "test").await.unwrap();
+        secondary(ctx).await;
     }
+}
+
+#[tokio::main]
+async fn secondary(ctx: Context) {
+    let html = Html::parse_document(&get("https://warthunder.com/en/news/").await.unwrap().text().await.unwrap());
+    let top_article_selector = "#bodyRoot > div.content > div:nth-child(2) > div > div > section > div > div.showcase__content-wrapper > div:nth-child(1)";
+    let top_article = html.select(&Selector::parse(top_article_selector).unwrap()).next().unwrap().text().collect::<String>();
+
+    ChannelId::from(866634236232597534).say(&ctx.http, top_article).await.unwrap();
 }
