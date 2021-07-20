1371d1e3440c051d43b43ea1a184030f4caf50ae
flareflo <florianlipp17@gmail.com>
2021-07-20 11:40:49 +0200

    Added debug info

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,8 @@ async fn main() {
 
         let my_http_client = Http::new_with_token(&token);
 
+        println!("{} {}", id, token);
+
         let webhook = my_http_client.get_webhook_with_token(id, &token)
             .await
             .expect("valid webhook");
