29f58298719da9ccdbebe1a2124a2c9ce4194407
FlareFlo <florianlipp17@gmail.com>
2021-07-20 17:01:18 +0200

    Changed refference type and removed unsafe print

--- src/main.rs
+++ src/main.rs
@@ -33,8 +33,7 @@ async fn main() {
 
         let my_http_client = Http::new_with_token(&token);
 
-        println!("{} {}", id, &token);
-        let webhook = match my_http_client.get_webhook_with_token(id, token.as_str()).await {
+        let webhook = match my_http_client.get_webhook_with_token(id, &token).await {
             Err(why) => {
                 println!("{}", why);
                 panic!("")
