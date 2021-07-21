185270886925f2c8878e0a0b9c71afbfeacb5397
flareflo <florianlipp17@gmail.com>
2021-07-21 09:41:52 +0200

    Broke out new file processor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod wt_news;
+
 use std::fs;
 use std::thread::sleep;
 use std::time;
@@ -7,12 +9,9 @@ use rand::Rng;
 use reqwest::get;
 use scraper::{Html, Selector};
 use serenity;
-use serenity::http::client::Http;
-// use serenity::model::id::{ChannelId, GuildId, WebhookId};
-// use serenity::model::prelude::{WebhookType};
 
-// use serenity::client::{ClientBuilder, EventHandler};
-// use serenity::model::channel::{Message, Embed};
+use serenity::http::client::Http;
+use crate::wt_news::html_processor;
 
 #[tokio::main]
 async fn main() {
@@ -41,7 +40,7 @@ async fn main() {
             Ok(hook) => hook,
         };
 
-        let content = html_procecss().await;
+        let content = html_processor().await;
         // let embed = Embed::fake(|mut e| {
         //     // e.title("Cool news and that shit");
         //     // e.description("Very nice");
@@ -71,48 +70,5 @@ async fn main() {
         }
     }
 
-    async fn html_procecss() -> String {
-        println!("Fetching data");
 
-        let url = "https://warthunder.com/en/news/";
-        let html = Html::parse_document(&get(url)
-            .await
-            .unwrap()
-            .text()
-            .await
-            .unwrap());
-        println!("Fetched data");
-
-        // let top_article_selector = Selector::parse("#bodyRoot > div.content > div:nth-child(2) > div > div > section > div > div.showcase__content-wrapper > div:nth-child(1)").unwrap();
-        let top_url_selector = Selector::parse("#bodyRoot > div.content > div:nth-child(2) > div > div > section > div > div.showcase__content-wrapper > div:nth-child(1) > a").unwrap();
-
-        // let top_article = html.select(&top_article_selector)
-        //     .next()
-        //     .unwrap()
-        //     .text()
-        //     .collect::<String>();
-        let top_url = html.select(&top_url_selector)
-            .next()
-            .unwrap()
-            .value()
-            .attr("href")
-            .unwrap();
-
-
-        // let top_article = top_article.replace("  ", "").replace("\n\n", "");
-        let keywords = vec![
-            "devblog", "event", "maintenance", "major", "trailer", "teaser", "developers",
-            "fixed", "vehicles", "economy", "changes", "sale", "twitch", "bundles", "development",
-            "shop", "special"
-        ];
-        let top_url = &*format!("https://warthunder.com{}", top_url);
-
-        for keyword in keywords {
-            if top_url.contains(keyword) {
-                println!("URL {} matched with keyword {}", top_url, keyword);
-                return (top_url).parse().unwrap();
-            }
-        }
-        return String::from("No match found");
-    }
 }
---
+++ src/wt_news.rs
@@ -0,0 +1,47 @@
+use scraper::{Html, Selector};
+use reqwest::get;
+
+pub async fn html_processor() -> String {
+    println!("Fetching data");
+
+    let url = "https://warthunder.com/en/news/";
+    let html = Html::parse_document(&get(url)
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap());
+    println!("Fetched data");
+
+    // let top_article_selector = Selector::parse("#bodyRoot > div.content > div:nth-child(2) > div > div > section > div > div.showcase__content-wrapper > div:nth-child(1)").unwrap();
+    let top_url_selector = Selector::parse("#bodyRoot > div.content > div:nth-child(2) > div > div > section > div > div.showcase__content-wrapper > div:nth-child(1) > a").unwrap();
+
+    // let top_article = html.select(&top_article_selector)
+    //     .next()
+    //     .unwrap()
+    //     .text()
+    //     .collect::<String>();
+    let top_url = html.select(&top_url_selector)
+        .next()
+        .unwrap()
+        .value()
+        .attr("href")
+        .unwrap();
+
+
+    // let top_article = top_article.replace("  ", "").replace("\n\n", "");
+    let keywords = vec![
+        "devblog", "event", "maintenance", "major", "trailer", "teaser", "developers",
+        "fixed", "vehicles", "economy", "changes", "sale", "twitch", "bundles", "development",
+        "shop", "special"
+    ];
+    let top_url = &*format!("https://warthunder.com{}", top_url);
+
+    for keyword in keywords {
+        if top_url.contains(keyword) {
+            println!("URL {} matched with keyword {}", top_url, keyword);
+            return (top_url).parse().unwrap();
+        }
+    }
+    return String::from("No match found");
+}
