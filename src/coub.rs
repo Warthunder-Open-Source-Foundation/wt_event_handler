62d55c3b864a96fbc3aeae03bc5776e7c2250acc
FlareFlo <florianlipp17@gmail.com>
2021-07-21 18:24:22 +0200

    Switched to tab indent from 4 spaces

--- src/coub.rs
+++ src/coub.rs
@@ -5,46 +5,46 @@ use scraper::{Html, Selector};
 use serde_json;
 
 pub async fn html_processor_coub() -> String {
-    #[derive(Default, Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
-    pub struct Root {
-        pub targets: Vec<Target>,
-    }
+	#[derive(Default, Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
+	pub struct Root {
+		pub targets: Vec<Target>,
+	}
 
-    #[derive(Default, Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
-    pub struct Target {
-        pub name: String,
-        pub recent_url: String,
-        pub domain: String,
-    }
+	#[derive(Default, Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
+	pub struct Target {
+		pub name: String,
+		pub recent_url: String,
+		pub domain: String,
+	}
 
-    let cache_raw = fs::read_to_string("recent.json").expect("Cannot read file");
-    let cache: Root = serde_json::from_str(&cache_raw).expect("Json cannot be read");
+	let cache_raw = fs::read_to_string("recent.json").expect("Cannot read file");
+	let cache: Root = serde_json::from_str(&cache_raw).expect("Json cannot be read");
 
-    let url = &cache.targets[1].domain;
+	let url = &cache.targets[1].domain;
 
 
-    println!("Fetching data from {}", url);
-    let html = Html::parse_document(&get(url)
-        .await
-        .unwrap()
-        .text()
-        .await
-        .unwrap());
-    println!("Fetched data with size of {} bytes", mem::size_of_val(&html));
+	println!("Fetching data from {}", url);
+	let html = Html::parse_document(&get(url)
+		.await
+		.unwrap()
+		.text()
+		.await
+		.unwrap());
+	println!("Fetched data with size of {} bytes", mem::size_of_val(&html));
 
-    // println!("{:?}", html);
+	// println!("{:?}", html);
 
-    // TODO fix selector to recieve proper html element
-    let top_url_selector = Selector::parse("body > div.body-container > div.logged.page-container > div.page__content > div.page__body.grid-container.gutter-gamma > div > div.coubs-list > div.coubs-list__inner.masonry > div.page > div:nth-child(1) > div.coub__inner > div > div.coub__description > div > div.description__info > h5 > a").unwrap();
+	// TODO fix selector to recieve proper html element
+	let top_url_selector = Selector::parse("body > div.body-container > div.logged.page-container > div.page__content > div.page__body.grid-container.gutter-gamma > div > div.coubs-list > div.coubs-list__inner.masonry > div.page > div:nth-child(1) > div.coub__inner > div > div.coub__description > div > div.description__info > h5 > a").unwrap();
 
-    let top_url = html.select(&top_url_selector)
-        .next()
-        .unwrap()
-        .value()
-        .attr("href")
-        .unwrap();
+	let top_url = html.select(&top_url_selector)
+		.next()
+		.unwrap()
+		.value()
+		.attr("href")
+		.unwrap();
 
-    println!("{}", top_url);
+	println!("{}", top_url);
 
-    "yes".to_string()
+	"yes".to_string()
 }
--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,9 @@ use serde_json;
 use serenity;
 use serenity::http::client::Http;
 
-use crate::wt_news::html_processor_wt_news;
 // use crate::coub::html_processor_coub;
 use crate::forum_news::html_processor_wt_forums;
+use crate::wt_news::html_processor_wt_news;
 
 mod wt_news;
 mod coub;
@@ -19,88 +19,85 @@ mod forum_news;
 
 #[tokio::main]
 async fn main() {
-    loop {
-        let wt_news_content = html_processor_wt_news(0).await;
-        handle_webhook(wt_news_content, 0).await;
+	loop {
+		let wt_news_content = html_processor_wt_news(0).await;
+		handle_webhook(wt_news_content, 0).await;
 
-        // let coub = html_processor_coub(1).await;
-        // handle_webhook(coub, 1).await;
+		// let coub = html_processor_coub(1).await;
+		// handle_webhook(coub, 1).await;
 
-        let forum_news = html_processor_wt_forums(2).await;
-        handle_webhook(forum_news, 2).await;
-
-
-
-        // Cool down to prevent rate limiting and excessive performance impact
-        let wait = rand::thread_rng().gen_range(50..70);
-        println!("Waiting for {} seconds", wait);
-        sleep(time::Duration::from_secs(wait))
-    }
-    async fn handle_webhook(content: String, index: usize) {
-
-        #[derive(Default, Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
-        pub struct Token {
-            token: String,
-        }
-
-        let token_raw = fs::read_to_string("assets/discord_token.json").expect("Cannot read file");
-        let token: Token = serde_json::from_str(&token_raw).expect("Json cannot be read");
-
-        let id = 867052162970288159;
-
-        let my_http_client = Http::new_with_token(&token.token);
-
-        let webhook = match my_http_client.get_webhook_with_token(id, &token.token).await {
-            Err(why) => {
-                println!("{}", why);
-                panic!("")
-            }
-            Ok(hook) => hook,
-        };
+		let forum_news = html_processor_wt_forums(2).await;
+		handle_webhook(forum_news, 2).await;
 
 
-        // let embed = Embed::fake(|mut e| {
-        //     // e.title("Cool news and that shit");
-        //     // e.description("Very nice");
-        //     e.url(content);
-        //     e
-        // });
+		// Cool down to prevent rate limiting and excessive performance impact
+		let wait = rand::thread_rng().gen_range(50..70);
+		println!("Waiting for {} seconds", wait);
+		sleep(time::Duration::from_secs(wait))
+	}
+	async fn handle_webhook(content: String, index: usize) {
+		#[derive(Default, Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
+		pub struct Token {
+			token: String,
+		}
 
-        #[derive(Default, Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
-        pub struct Root {
-            pub targets: Vec<Target>,
-        }
+		let token_raw = fs::read_to_string("assets/discord_token.json").expect("Cannot read file");
+		let token: Token = serde_json::from_str(&token_raw).expect("Json cannot be read");
 
-        #[derive(Default, Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
-        pub struct Target {
-            pub name: String,
-            pub recent_url: String,
-            pub domain: String,
-        }
+		let id = 867052162970288159;
+
+		let my_http_client = Http::new_with_token(&token.token);
+
+		let webhook = match my_http_client.get_webhook_with_token(id, &token.token).await {
+			Err(why) => {
+				println!("{}", why);
+				panic!("")
+			}
+			Ok(hook) => hook,
+		};
 
 
-        let cache_raw = fs::read_to_string("recent.json").expect("Cannot read file");
-        let mut cache: Root = serde_json::from_str(&cache_raw).expect("Json cannot be read");
+		// let embed = Embed::fake(|mut e| {
+		//     // e.title("Cool news and that shit");
+		//     // e.description("Very nice");
+		//     e.url(content);
+		//     e
+		// });
 
-        if !content.contains("No match found") {
-            if cache.targets[index].recent_url != content {
-                println!("New post found, hooking now");
-                webhook.execute(&my_http_client, false, |w| {
-                    w.content(&format!("[{a}]({a})", a = content));
-                    w.username("The WT news bot");
-                    // w.embeds(vec![embed]);
-                    w
-                })
-                    .await
-                    .unwrap();
-                cache.targets[index].recent_url = content;
-                let write = serde_json::to_string(&cache).unwrap();
-                fs::write("recent.json", write).expect("Couldn't write to file");
-            } else {
-                println!("Content was recently fetched and is not new");
-            }
-        } else {
-            println!("Content was either not a match")
-        }
-    }
+		#[derive(Default, Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
+		pub struct Root {
+			pub targets: Vec<Target>,
+		}
+
+		#[derive(Default, Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
+		pub struct Target {
+			pub name: String,
+			pub recent_url: String,
+			pub domain: String,
+		}
+
+		let cache_raw = fs::read_to_string("recent.json").expect("Cannot read file");
+		let mut cache: Root = serde_json::from_str(&cache_raw).expect("Json cannot be read");
+
+		if !content.contains("No match found") {
+			if cache.targets[index].recent_url != content {
+				println!("New post found, hooking now");
+				webhook.execute(&my_http_client, false, |w| {
+					w.content(&format!("[{a}]({a})", a = content));
+					w.username("The WT news bot");
+					// w.embeds(vec![embed]);
+					w
+				})
+					.await
+					.unwrap();
+				cache.targets[index].recent_url = content;
+				let write = serde_json::to_string(&cache).unwrap();
+				fs::write("recent.json", write).expect("Couldn't write to file");
+			} else {
+				println!("Content was recently fetched and is not new");
+			}
+		} else {
+			println!("Content was either not a match")
+		}
+	}
 }
