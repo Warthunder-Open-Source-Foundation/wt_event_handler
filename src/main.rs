fae6322d0400c07424c0867c98123b2afed02e9a
flareflo <florianlipp17@gmail.com>
2021-07-21 10:21:41 +0200

    Partial json parsing

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ use rand::Rng;
 use reqwest::get;
 use scraper::{Html, Selector};
 use serenity;
+use serde::{Deserialize, Serialize};
+use serde_json;
 
 use serenity::http::client::Http;
 use crate::wt_news::html_processor_wt_news;
@@ -49,26 +51,33 @@ async fn main() {
         //     e
         // });
 
-        let recent = fs::read_to_string("recent.txt").expect("Cannot read file");
-
-        if !content.contains("No match found") {
-            if recent != content {
-                println!("New post found, hooking now");
-                webhook.execute(&my_http_client, false, |w| {
-                    fs::write("recent.txt", &content).expect("Writing to recent file failed");
-                    w.content(&format!("[{a}]({a})", a = content));
-                    w.username("The WT news bot");
-                    // w.embeds(vec![embed]);
-                    w
-                })
-                    .await
-                    .unwrap();
-            }else {
-                println!("Content was recently fetched and is not new");
-            }
-        } else {
-            println!("Content was either not a match")
+        #[derive(Serialize, Deserialize)]
+        struct Recent {
+            url: String,
         }
+
+        let recent = fs::read_to_string("recent.json").expect("Cannot read file");
+        let testjet: Recent = serde_json::from_str(&recent).expect("Json cannot be read");
+        println!("{}", testjet.url);
+
+        // if !content.contains("No match found") {
+        //     if recent != content {
+        //         println!("New post found, hooking now");
+        //         webhook.execute(&my_http_client, false, |w| {
+        //             fs::write("recent.txt", &content).expect("Writing to recent file failed");
+        //             w.content(&format!("[{a}]({a})", a = content));
+        //             w.username("The WT news bot");
+        //             // w.embeds(vec![embed]);
+        //             w
+        //         })
+        //             .await
+        //             .unwrap();
+        //     }else {
+        //         println!("Content was recently fetched and is not new");
+        //     }
+        // } else {
+        //     println!("Content was either not a match")
+        // }
     }
 
 
