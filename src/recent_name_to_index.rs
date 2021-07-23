c2fdafaa305e544711983f3ba4c2c9410f1146f3
flareflo <florianlipp17@gmail.com>
2021-07-23 12:49:29 +0200

    Fixed proper index resolve

--- src/recent_name_to_index.rs
+++ src/recent_name_to_index.rs
@@ -16,18 +16,13 @@ pub fn convert(name: &str) -> usize {
 	let cache_raw = fs::read_to_string("recent.json").expect("Cannot read file");
 	let cache: Root = serde_json::from_str(&cache_raw).expect("Json cannot be read");
 
+	let index: Option<usize>;
+	index = cache.targets.iter().position(|r| r.name == name);
 
-	// This is fucking retarded, hard coding a useless vector because iterating over it using length is not possible
-	let iter = [0, 1, 2];
-	let mut result: usize = usize::MAX;
-	for targets in iter {
-		if name == cache.targets[targets].name {
-			result = targets;
-		}
-	}
-	if result == usize::MAX {
-		println!("Could not resolve index for {}", name);
+	if index.is_none() {
+		println!("Index could not be resolved");
 		panic!();
 	}
-	return result;
+
+	return index.unwrap();
 }
