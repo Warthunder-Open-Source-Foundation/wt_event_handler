2470825537979109e4253e78461663ad457228bc
FlareFlo <florianlipp17@gmail.com>
2021-08-04 23:51:04 +0200

    Changelog ignoring pins too now
    Took 3 minutes

--- src/scrapers/wt_changelog.rs
+++ src/scrapers/wt_changelog.rs
@@ -1,8 +1,9 @@
-use std::{fs};
+use std::fs;
 
 use log::*;
 use reqwest::get;
 use scraper::{Html, Selector};
+
 use crate::json_to_structs::recent::*;
 
 pub async fn html_processor_wt_changelog() -> Option<String> {
@@ -12,6 +13,7 @@ pub async fn html_processor_wt_changelog() -> Option<String> {
 	let url = &recent.warthunder_changelog.domain;
 
 	println!("Fetching data from {}", url);
+	info!("Fetching data from {}", url);
 
 	if get(url).await.is_err() {
 		println!("Cannot fetch data");
@@ -26,12 +28,47 @@ pub async fn html_processor_wt_changelog() -> Option<String> {
 		.await
 		.unwrap());
 
-	let top_url_selector = Selector::parse("#bodyRoot > div.content > div:nth-child(2) > div:nth-child(2) > div > section > div > div.showcase__content-wrapper > div:nth-child(2) > a").unwrap();
+	// Too lazy to make !format macro
+	let selectors = [
+		Selector::parse("#bodyRoot > div.content > div:nth-child(2) > div:nth-child(2) > div > section > div > div.showcase__content-wrapper > div:nth-child(1) > a").unwrap(),
+		Selector::parse("#bodyRoot > div.content > div:nth-child(2) > div:nth-child(2) > div > section > div > div.showcase__content-wrapper > div:nth-child(2) > a").unwrap()
+	];
+	let pin = Selector::parse("#bodyRoot > div.content > div:nth-child(2) > div:nth-child(2) > div > section > div > div.showcase__content-wrapper > div:nth-child(1) > div.widget__pin").unwrap();
 
-	 if let Some(top_url) = html.select(&top_url_selector).next() {
-		let top_url = &*format!("https://warthunder.com{}", top_url.value().attr("href").unwrap());
-		 Some(top_url.to_string())
+	let mut top_url: Vec<String> = vec!["".to_string(), "".to_string()];
+
+	if let Some(x) = html.select(&selectors[0]).next() {
+		top_url[0] = (&*format!("https://warthunder.com{}", x.value().attr("href").unwrap())).parse().unwrap();
 	} else {
+		return fetch_failed();
+	};
+	if let Some(x) = html.select(&selectors[1]).next() {
+		top_url[1] = (&*format!("https://warthunder.com{}", x.value().attr("href").unwrap())).parse().unwrap();
+	} else {
+		return fetch_failed();
+	};
+
+	if let Some(pin_url) = html.select(&pin).next() {
+		let pin_url = pin_url.value().attr("class").unwrap();
+		if pin_url == "widget__pin"  {
+			return Some(pinned(recent, &top_url).clone())
+		} else {
+			return Some(top_url[0].clone())
+		}
+	} else {
+		return fetch_failed()
+	}
+
+	fn pinned(recent: Recent, top_url: &Vec<String>) -> &String {
+		let recents = &recent.warthunder_changelog.recent_url;
+		if !recents.contains(&top_url[0]) {
+			&top_url[0]
+		} else {
+			&top_url[1]
+		}
+	}
+
+	fn fetch_failed() -> Option<String> {
 		println!("Fetch failed");
 		error!("Fetch failed");
 		None
