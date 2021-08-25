5a0187603eaa494a50e658849b00f9596d14283a
FlareFlo <florianlipp17@gmail.com>
2021-08-25 16:30:36 +0200

    Outsourced functions for scrapers
    Took 26 minutes

---
+++ src/scrapers/scraper_resources/resources.rs
@@ -0,0 +1,45 @@
+use std::fs;
+
+use scraper::Html;
+use reqwest::get;
+use log::{info, error};
+
+use crate::json_to_structs::recent::Recent;
+
+pub fn get_local() -> Recent {
+	let cache_raw = fs::read_to_string("assets/recent.json").expect("Cannot read file");
+	let cache: Recent = serde_json::from_str(&cache_raw).expect("Json cannot be read");
+	cache
+}
+
+pub async fn request_html(url: &str) -> Option<Html> {
+	println!("Fetching data from {}", &url);
+	info!("Fetching data from {}", &url);
+
+	let html;
+	if let Ok(raw_html) = get(url).await {
+		if let Ok(text) = raw_html.text().await {
+			html = Html::parse_document(text.as_str());
+			return Some(html)
+		} else {
+			return None;
+		}
+	} else {
+		return None;
+	}
+}
+
+pub fn fetch_failed() -> Option<String> {
+	println!("Fetch failed");
+	error!("Fetch failed");
+	None
+}
+
+pub fn pinned<'a>(recent: &'a Recent, top_url: &'a [String]) -> &'a String {
+	let recents = &recent.warthunder_changelog.recent_url;
+	if recents.contains(&top_url[0]) {
+		&top_url[1]
+	} else {
+		&top_url[0]
+	}
+}
--- src/scrapers/wt_changelog.rs
+++ src/scrapers/wt_changelog.rs
@@ -1,33 +1,22 @@
-use std::fs;
-
 use log::{error, info};
-use reqwest::get;
-use scraper::{Html, Selector};
+use scraper::{Selector};
 
 use crate::json_to_structs::recent::Recent;
+use crate::scrapers::scraper_resources::resources::{get_local, request_html, fetch_failed, pinned};
 
 pub async fn html_processor_wt_changelog() -> Option<String> {
-	let cache_raw_recent = fs::read_to_string("assets/recent.json").expect("Cannot read file");
-	let recent: Recent = serde_json::from_str(&cache_raw_recent).expect("Json cannot be read");
+	let recent = get_local();
 
 	let url = &recent.warthunder_changelog.domain;
 
-	println!("Fetching data from {}", url);
-	info!("Fetching data from {}", url);
-
 	let html;
-
-	if let Ok(raw_html) = get(url).await {
-		if let Ok(text) = raw_html.text().await {
-			html = Html::parse_document(text.as_str());
-		} else {
-			return None;
-		}
-	} else {
-		return None;
+	if let Some(value) = request_html(&url).await{
+		html = value;
+	}else {
+		return fetch_failed()
 	}
 
-	// Too lazy to make !format macro
+
 	let selectors = [
 		Selector::parse("#bodyRoot > div.content > div:nth-child(2) > div:nth-child(2) > div > section > div > div.showcase__content-wrapper > div:nth-child(1) > a").unwrap(),
 		Selector::parse("#bodyRoot > div.content > div:nth-child(2) > div:nth-child(2) > div > section > div > div.showcase__content-wrapper > div:nth-child(2) > a").unwrap()
@@ -55,19 +44,4 @@ pub async fn html_processor_wt_changelog() -> Option<String> {
 		return Some(top_url[0].clone());
 	}
 	return fetch_failed();
-}
-
-fn pinned<'a>(recent: &'a Recent, top_url: &'a [String]) -> &'a String {
-	let recents = &recent.warthunder_changelog.recent_url;
-	if recents.contains(&top_url[0]) {
-		&top_url[1]
-	} else {
-		&top_url[0]
-	}
-}
-
-fn fetch_failed() -> Option<String> {
-	println!("Fetch failed");
-	error!("Fetch failed");
-	None
 }
