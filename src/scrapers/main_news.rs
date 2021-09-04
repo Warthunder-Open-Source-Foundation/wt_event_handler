69742284db492f0f961d59985a26eb6fdbb89cbe
FlareFlo <florianlipp17@gmail.com>
2021-09-04 23:25:07 +0200

    Fixed clippy loop
    Took 6 minutes

--- src/scrapers/main_news.rs
+++ src/scrapers/main_news.rs
@@ -3,7 +3,7 @@ use std::option::Option::Some;
 use log::error;
 
 use crate::json_to_structs::recent::{format_selector, Value};
-use crate::scrapers::scraper_resources::resources::{fetch_failed, request_html, format_result, ScrapeType, pin_loop};
+use crate::scrapers::scraper_resources::resources::{fetch_failed, format_result, pin_loop, request_html, ScrapeType};
 
 pub async fn html_processor(recent_value: &Value, scrape_type: ScrapeType) -> Option<String> {
 	let url = &recent_value.domain;
@@ -22,9 +22,8 @@ pub async fn html_processor(recent_value: &Value, scrape_type: ScrapeType) -> Op
 	let top_url_selector = format_selector(&recent_value, "selector", post);
 	if let Some(top_url) = html.select(&top_url_selector).next() {
 		return Some(format_result(top_url, scrape_type));
-	} else {
-		println!("Fetch failed");
-		error!("Fetch failed");
-		return None
-	};
+	}
+	println!("Fetch failed");
+	error!("Fetch failed");
+	return None;
 }
