472416f7edc2cb61945a6f34dd2e522404f10066
FlareFlo <mail@flareflo.dev>
2022-03-13 02:07:48 +0100

    Light safeties

--- src/scrapers/main_news.rs
+++ src/scrapers/main_news.rs
@@ -33,17 +33,17 @@ pub async fn html_processor(recent_value: &Channel, scrape_type: ScrapeType) -> 
 		let (title, img_url, preview_text) = match scrape_type {
 			ScrapeType::Forum => {
 				(
-					post_html.select(&Selector::parse("head>meta:nth-child(5)").unwrap()).next().unwrap().value().attr("content").unwrap_or("").to_string(),
+					post_html.select(&Selector::parse("head>meta:nth-child(5)").unwrap_or(Selector::parse("html").unwrap())).next().unwrap().value().attr("content").unwrap_or("").to_string(),
 					"".to_string(),
-					post_html.select(&Selector::parse("head>meta:nth-child(8)").unwrap()).next().unwrap().value().attr("content").unwrap_or("").to_string()
+					post_html.select(&Selector::parse("head>meta:nth-child(8)").unwrap_or(Selector::parse("meta").unwrap())).next().unwrap().value().attr("content").unwrap_or("").to_string()
 				)
 			}
 			ScrapeType::Main => {
 				(
-					post_html.select(&Selector::parse("head>meta:nth-child(13)").unwrap()).next().unwrap().value().attr("content").unwrap_or("").to_string(),
+					post_html.select(&Selector::parse("head>meta:nth-child(13)").unwrap_or(Selector::parse("html").unwrap())).next().unwrap().value().attr("content").unwrap_or("").to_string(),
 					{
 						let mut actual= "".to_owned();
-						let _ = post_html.select(&Selector::parse("img").unwrap()).for_each(|item|{
+						let _ = post_html.select(&Selector::parse("img").unwrap_or(Selector::parse("html").unwrap())).for_each(|item|{
 							if let Some(proper_image) = item.value().attr("src") {
 								actual = proper_image.to_owned();
 							}
@@ -55,10 +55,10 @@ pub async fn html_processor(recent_value: &Channel, scrape_type: ScrapeType) -> 
 			}
 			ScrapeType::Changelog => {
 				(
-					post_html.select(&Selector::parse("head>meta:nth-child(13)").unwrap()).next().unwrap().value().attr("content").unwrap_or("").to_string(),
+					post_html.select(&Selector::parse("head>meta:nth-child(13)").unwrap_or(Selector::parse("html").unwrap())).next().unwrap().value().attr("content").unwrap_or("").to_string(),
 					{
 						let mut actual= "".to_owned();
-						let _ = post_html.select(&Selector::parse("img").unwrap()).for_each(|item|{
+						let _ = post_html.select(&Selector::parse("img").unwrap_or(Selector::parse("html").unwrap())).for_each(|item|{
 							if let Some(proper_image) = item.value().attr("src") {
 								actual = proper_image.to_owned();
 							}
